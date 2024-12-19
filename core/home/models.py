7c6c819b52e7a5760e8a5d0d056cfc85abadf0e1
unknown <sisodiyas943@gmail.com>
2024-12-19 18:56:55 +0530

    5 models and migrations, sqlite3

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True,blank=True)
+    image = models.ImageField()
+    file = models.FileField()
+# refer djnago model filed
+
+class Product(models.Model):
+    pass
